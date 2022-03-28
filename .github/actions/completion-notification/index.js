4b0283d711a0cbd76ee29a9e854227e0dd4aa954
Quentin Lintz <quentin@stripe.com>
2022-03-28 19:57:19 -0400

    Add repo and user name inputs to the action

--- .github/actions/completion-notification/index.js
+++ .github/actions/completion-notification/index.js
@@ -1,30 +1,32 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-const SLACK_URL = core.getInput('slack-webhook-url');
-const repositoryName = process.env.GITHUB_REPOSITORY;
-const authorName = process.env.GITHUB_ACTOR;
+const SLACK_URL = core.getInput("slack-webhook-url");
+const repositoryName = core.getInput("repo-name");
+const authorName = core.getInput("user-name");
 
 /**
  * Liberally borrowed from tiloio/slack-webhook-action [0],
  * but making our own copy to (a) simplify, and (b) reduce
  * external dependencies.
- * 
+ *
  * [0] https://github.com/tiloio/slack-webhook-action/blob/main/index.js
  */
 (async () => {
   try {
     const payload = JSON.stringify({
-      hello: 'world'
+      hello: "world",
     });
     const result = await sendSlackNotification(payload);
-    if (result !== 'ok') {
-      if (result === 'invalid_payload') {
-          core.setFailed('Could not send notification with invalid payload: ' + data);
+    if (result !== "ok") {
+      if (result === "invalid_payload") {
+        core.setFailed(
+          "Could not send notification with invalid payload: " + data
+        );
       } else {
-          core.setFailed('Could not send notification: ' + result);
+        core.setFailed("Could not send notification: " + result);
       }
-  }
+    }
   } catch (err) {
     core.setFailed(error.message);
   }
@@ -35,20 +37,20 @@ function sendSlackNotification(payload) {
     const request = https.request(
       SLACK_URL,
       {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-              'Content-Length': payload.length,
-          }
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": payload.length,
+        },
       },
-      response => {
-          response.on('data', responseData => resolve(responseData.toString()));
-          response.on("error", error => reject(error));
+      (response) => {
+        response.on("data", (responseData) => resolve(responseData.toString()));
+        response.on("error", (error) => reject(error));
       }
-  );
+    );
 
-  request.on('error', error => reject(error));
-  request.write(payload);
-  request.end();
-  })
+    request.on("error", (error) => reject(error));
+    request.write(payload);
+    request.end();
+  });
 }

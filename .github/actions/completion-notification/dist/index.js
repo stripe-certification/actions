562333c6fd9044e9c4d9640a1b015704bf772762
Quentin Lintz <quentin@stripe.com>
2022-03-29 11:45:14 -0400

    Remove the emoji

--- .github/actions/completion-notification/dist/index.js
+++ .github/actions/completion-notification/dist/index.js
@@ -17,6 +17,73 @@ function getPayload(user, repo) {
           emoji: true,
         },
       },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Visit their solution repo:",
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "GitHub Repo",
+            emoji: true,
+          },
+          value: "click_me_123",
+          url: repoUrl,
+          action_id: "button-action",
+        },
+      },
+      {
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "React with eyes to let teammates know you've taken this notification, then complete the following steps",
+        },
+        accessory: {
+          type: "checkboxes",
+          options: [
+            {
+              text: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              description: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              value: "value-0",
+            },
+            {
+              text: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              description: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              value: "value-1",
+            },
+            {
+              text: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              description: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              value: "value-2",
+            },
+          ],
+          action_id: "checkboxes-action",
+        },
+      },
     ],
   };
 

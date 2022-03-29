61337fbe7cbf6d11f68a8b3e2c62f43a08294efc
Quentin Lintz <quentin@stripe.com>
2022-03-29 11:39:09 -0400

    Add test payload

--- .github/actions/completion-notification/dist/index.js
+++ .github/actions/completion-notification/dist/index.js
@@ -13,8 +13,74 @@ function getPayload(user, repo) {
         type: "header",
         text: {
           type: "plain_text",
-          text: userMessage,
+          text: "This is a header block",
           emoji: true,
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "This is a section block with a button.",
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me",
+            emoji: true,
+          },
+          value: "click_me_123",
+          action_id: "button-action",
+        },
+      },
+      {
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "This is a section block with checkboxes.",
+        },
+        accessory: {
+          type: "checkboxes",
+          options: [
+            {
+              text: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              description: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              value: "value-0",
+            },
+            {
+              text: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              description: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              value: "value-1",
+            },
+            {
+              text: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              description: {
+                type: "mrkdwn",
+                text: "*this is mrkdwn text*",
+              },
+              value: "value-2",
+            },
+          ],
+          action_id: "checkboxes-action",
         },
       },
     ],

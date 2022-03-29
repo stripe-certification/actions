1697a6ede9c538b1bc7b3f6ec68619943e45640e
Quentin Lintz <quentin@stripe.com>
2022-03-29 11:36:46 -0400

    Only include the header

--- .github/actions/completion-notification/dist/index.js
+++ .github/actions/completion-notification/dist/index.js
@@ -17,34 +17,6 @@ function getPayload(user, repo) {
           emoji: true,
         },
       },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: "You can find their solution repo here:",
-        },
-        accessory: {
-          type: "button",
-          text: {
-            type: "plain_text",
-            text: "GitHub Repo",
-            emoji: true,
-          },
-          value: "click_me_123",
-          url: repoUrl,
-          action_id: "button-action",
-        },
-      },
-      {
-        type: "divider",
-      },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: "Please react with 👀 to let teammates know you are provisioning, then complete the following steps:",
-        },
-      },
     ],
   };
 
